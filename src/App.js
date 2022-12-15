b2ac886a92cd4f0b960ceb85b972112e1081f830
Paul <paul.siderius@gmail.com>
2022-12-16 11:00:54 +1300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hello World.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn to code bro
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
